4f19e25c327e03c7526877b7536745bf814b8c2c
jabateIC <jabate@infinite-cooling.com>
2022-07-06 09:52:34 -0400

    Adding in git files

---
+++ main.py
@@ -0,0 +1,21 @@
+import modbus_tk
+import bacpypes
+import serial
+import kivy
+
+'''
+Objective:
+
+Create a Master/Slave example in the terminal that communicates using Modbus & BACnet, ideally at the same time.
+* Potentially need a Ethernet port for this
+* Raspberry Pi will act as slave, computer can suffice as both sides for now
+* Start with Modbus, add BACnet
+'''
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
